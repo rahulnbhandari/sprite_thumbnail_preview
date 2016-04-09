ff5de8ca79ffe828b9dcc46667deab3bbd5dbc69
rahul <rahulnbhandari@gmail.com>
2016-04-09 18:47:31 -0400

    Added files via upload

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# sprite_thumbnail_preview
-thumbnail preview/sprite eneratorg
+# JWPlayer thumbnail preview generator
+
+A simple but effective command-line tool for generating thumbnails of a video and corresponding VTT file for use within JW Player to generate the toolbar thumbnail previews.
+
+[![Flattr this git repo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=acollington&url=https://github.com/amnuts/jwplayer-thumbnail-preview-generator&title=JWPlayer%20thumbnail%20preview%20generator&language=&tags=github&category=software)
+
+### Requirements
+
+FFmpeg is required to generate the thumbnails.  If you have libav-tools install and the binary is a different name to ffmpeg (or even if you have ffmpeg but installed in a different location) then you can modify the `$params` information to point to your particular ffmpeg install.
+
+PHP 5.4+ is required as the code uses short array syntax and the CallbackFilterIterator.
+
+### Getting started
+
+There are two ways to getting started using this script:
+
+1. Simply to copy/paste or download the thumbnails.php to your server
+2. Install via composer by running the command:
+```bash
+composer require rahulnbhandari/sprite_thumbnail_preview
+```
+
+### How to use the generator
+
+#### Simplest example
+
+Typical usage would look like:
+
+```bash
+php thumbnails.php -i "/input/video.mp4"
+```
+
+This would generate the thumbnails for the video given coalesced into one file (a sprite sheet) and the required VTT file that specifies which thumbnail to display and where in the sprite sheet it is located.
+
+#### Verbose output
+
+If you do not wish to generate a sprite sheet then include the `-v` switch to generate verbose output - ie, all the thumbnails are separate files and the VTT file points to each.
+
+```bash
+php thumbnails.php -i "/input/video.mp4" -v
+```
+
+It is recommended that you use the default coalesced file as this is more optimal when loading than individual images.
+
+#### Changing output directory
+
+You can change the output directory with the `-o` switch:
+
+```bash
+php thumbnails.php -i "/input/video.mp4" -o "/output/directory"
+```
+
+This will write the images and VTT file in the provided directory (default is just to write into the same directory as the generator script).
+
+#### Change time between thumbnails
+
+To alter the default time between thumbnails use the `-t` switch with the number of seconds you'd like between each:
+
+```bash
+php thumbnails.php -i "/input/video.mp4" -t 30
+```
+
+That will generate one thumbnail for every 30 seconds of video.
+
+#### Change thumbnail width
+
+To change the width of the thumbnail use the `-w` switch with the size in pixels:
+
+```bash
+php thumbnails.php -i "/input/video.mp4" -w 75
+```
+
+That will generate thumbnails that are 75 pixels in width.  The height is automatic and proportional to the video size.
+
+#### Generate poster image
+
+The tool also provides the ability to generate a poster file of the video from a random frame of the video at the same time it's generating the thumbnails.  To do this, use the `-p` switch:
+
+```bash
+php thumbnails.php -i "/input/video.mp4" -p
+```
+
+### How to include in JW Player
+
+The code you would use for JW Player would be something like:
+
+```html
+<div id="video">video loading...</div>
+<script>
+    jwplayer("video").setup({
+        playlist: [{
+            file: "/input/video.mp4",
+            image: "poster.jpg",
+            tracks: [{
+                file: "thumbnails.vtt",
+                kind: "thumbnails"
+            }]
+        }]
+    });
+</script>
---
+++ thumbnails.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * Video thumbnail preview generator
+ *
+ * A simple but effective command-line tool for generating thumbnails of a
+ * video and corresponding VTT file for use within JW Player.
+ *
+ * This script requires that ffmpeg is installed on your system.
+ *
+ * @author Rahul Bhandari, rahulnbhandari@gmail.com
+ */
+
+$params = [
+    'ffmpeg'      => 'ffmpeg', // the ffmpeg command - full path if needs be
+    'input'       => null,     // input video file - specified as command line parameter
+    'output'      => __DIR__,  // The output directory
+    'timespan'    => 10,       // seconds between each thumbnail
+    'thumbWidth'  => 120,      // thumbnail width
+    'spriteWidth' => 10        // number of thumbnails per row in sprite sheet
+];
+
+$commands = [
+    'details' => $params['ffmpeg'] . ' -i %s 2>&1',
+    'poster'  => $params['ffmpeg'] . ' -ss %d -i %s -y -vframes 1 "%s/poster.jpg" 2>&1',
+    'thumbs'  => $params['ffmpeg'] . ' -ss %0.04f -i %s -y -an -sn -vsync 0 -q:v 5 -threads 1 '
+        . '-vf scale=%d:-1,select="not(mod(n\\,%d))" "%s/thumbnails/%s-%%04d.jpg" 2>&1'
+];
+
+define('EX_USAGE', 64);
+define('EX_NOINPUT', 66);
+define('EX_UNAVAILABLE', 69);
+define('EX_CANTCREAT', 73);
+
+if (PHP_SAPI !== 'cli') {
+    echo 'Sorry, you can only use this via the command line.';
+    exit(EX_USAGE);
+}
+
+$opts = getopt('i:o:t:w:hvpd');
+if (isset($opts['h']) || !isset($opts['i'])) {
+    echo <<< EOT
+Usage: php thumbnails.php -i "/input/video.mp4" [-o "/output/directory"] [-t <number>] [-w <number>] [-v] [-p] [-d]
+
+    -i: The input file to be used.
+    -o: The output directory where the thumbnails and vtt file will be saved
+    -t: The time span (in seconds) between each thumbnail (default, {$params['timespan']})
+    -w: The max width of the thumbnail (default, {$params['thumbWidth']})
+    -v: Verbose - don't coalesce the thumbnails into one image
+    -p: Generate poster image from a random frame in the video
+    -d: Delete any previous thumbnails that match before generating new images
+    -h: Show this message
+
+EOT;
+    exit(EX_USAGE);
+}
+
+// process input parameters
+
+$params['input'] = escapeshellarg($opts['i']);
+if (isset($opts['o'])) {
+    $params['output'] = realpath($opts['o']);
+}
+if (isset($opts['t']) && (int)$opts['t']) {
+    $params['timespan'] = $opts['t'];
+}
+if (isset($opts['w']) && (int)$opts['w']) {
+    $params['thumbWidth'] = $opts['w'];
+}
+
+// sanity checks
+
+if (!is_readable($opts['i'])) {
+    echo "Cannot read the input file '{$opts['i']}'";
+    exit(EX_NOINPUT);
+}
+if (!is_writable($params['output'])) {
+    echo "Cannot write to output directory '{$opts['o']}'";
+    exit(EX_CANTCREAT);
+}
+if (!file_exists($params['output'] . '/thumbnails')) {
+    if (!mkdir($params['output'] . '/thumbnails')) {
+        echo "Could not create thumbnail output directory '{$params['output']}/thumbnails'";
+        exit(EX_CANTCREAT);
+    }
+}
+$details = shell_exec(sprintf($commands['details'], $params['input']));
+if ($details === null || !preg_match('/^(?:\s+)?ffmpeg version ([^\s,]*)/i', $details)) {
+    echo 'Cannot find ffmpeg - try specifying the path in the $params variable';
+    exit(EX_UNAVAILABLE);
+}
+
+// determine some required values
+
+$time = $tbr = [];
+preg_match('/Duration: ((\d+):(\d+):(\d+))\.\d+, start: ([^,]*)/is', $details, $time);
+preg_match('/\b(\d+(?:\.\d+)?) tbr\b/', $details, $tbr);
+
+if (empty($time) || empty($tbr)) {
+    echo 'Cannot determine the duration or video frame rate - both are required to create the thumbnails.';
+    exit(EX_UNAVAILABLE);
+}
+
+$duration = ($time[2] * 3600) + ($time[3] * 60) + $time[4];
+$start = $time[5];
+$tbr = $tbr[1];
+
+// generate random poster if required
+
+if (isset($opts['p'])) {
+    shell_exec(sprintf($commands['poster'], rand(1, $duration - 1), $opts['i'], $params['output']));
+}
+
+// generate all thumbnail images
+
+$name = strtolower(substr(basename($opts['i']), 0, strrpos(basename($opts['i']), '.')));
+$filter = function($current, $key, $iterator) use ($name) {
+    return (
+        $current->isFile()
+        && preg_match("!{$name}-\\d{4}\\.jpg$!", $current->getFilename())
+    );
+};
+
+if (isset($opts['d'])) {
+    $files = new CallbackFilterIterator(
+        new FilesystemIterator("{$params['output']}/thumbnails"), $filter
+    );
+    foreach ($files as $f) {
+        unlink($f);
+    }
+}
+shell_exec(sprintf($commands['thumbs'],
+    $start + .0001, $params['input'], $params['thumbWidth'],
+    $params['timespan'] * $tbr, $params['output'], $name
+));
+$files = array_values(iterator_to_array(
+    new CallbackFilterIterator(
+        new FilesystemIterator("{$params['output']}/thumbnails"), $filter
+    )
+));
+if (!($total = count($files))) {
+    echo "Could not find any thumbnails matching '{$params['output']}/thumbnails/{$name}-\\d{4}.jpg'";
+    exit(EX_NOINPUT);
+}
+sort($files, SORT_NATURAL);
+
+// create coalesce image if needs be
+
+if (!isset($opts['v'])) {
+    $thumbsAcross = min($total, $params['spriteWidth']);
+    $sizes = getimagesize($files[0]);
+    $rows = ceil($total/$thumbsAcross);
+    $w = $sizes[0] * $thumbsAcross;
+    $h = $sizes[1] * $rows;
+    $coalesce = imagecreatetruecolor($w, $h);
+}
+
+// generate vtt file, merge thumbnails if needs be
+
+$vtt = "WEBVTT\n\n";
+for ($rx = $ry = $s = $f = 0; $f < $total; $f++) {
+    $t1 = sprintf('%02d:%02d:%02d.000', ($s / 3600), ($s / 60 % 60), $s % 60);
+    $s += $params['timespan'];
+    $t2 = sprintf('%02d:%02d:%02d.000', ($s / 3600), ($s / 60 % 60), $s % 60);
+    if (isset($opts['v'])) {
+        $vtt .= "{$t1} --> {$t2}\nthumbnails/" . basename($files[$f]);
+    } else {
+        if ($f && !($f % $thumbsAcross)) {
+            $rx = 0;
+            ++$ry;
+        }
+        imagecopymerge($coalesce, imagecreatefromjpeg($files[$f]), $rx * $sizes[0], $ry * $sizes[1], 0, 0, $sizes[0], $sizes[1], 100);
+        $vtt .= sprintf("%s --> %s\nthumbnails.jpg#xywh=%d,%d,%d,%d", $t1, $t2, $rx++ * $sizes[0], $ry * $sizes[1],  $sizes[0], $sizes[1]);
+    }
+    $vtt .= "\n\n";
+}
+
+// tidy up
+
+if (!isset($opts['v'])) {
+    imagejpeg($coalesce, "{$params['output']}/thumbnails.jpg", 90);
+    for ($s = 0, $f = 0; $f < $total; $f++) {
+        unlink($files[$f]);
+    }
+}
+
+file_put_contents("{$params['output']}/thumbnails.vtt", $vtt);
+echo "Process finished. Check the output directory '{$params['output']}' for VTT file and images";
